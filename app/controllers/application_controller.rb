85952359962c4819c625b109fbe8b9fa63cdae7a
Daniel <danielgunawann1997@gmail.com>
2020-12-13 03:48:52 +0700

    Add role to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-	protect_from_forgery with: :exception
+protect_from_forgery with: :exception
 
   before_action :update_allowed_parameters, if: :devise_controller?
 
@@ -8,5 +8,17 @@ class ApplicationController < ActionController::Base
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :phone_number, :address, :email, :password)}
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :surname, :email, :password, :current_password)}
+  end
+
+  def after_sign_in_path_for(resource)
+    # check for the class of the object to determine what type it is
+    case resource.role
+    when "admin"
+      dashboard_index_path
+    when "customer"
+      homepage_index_path
+    else
+      root_path
+    end
   end
 end
