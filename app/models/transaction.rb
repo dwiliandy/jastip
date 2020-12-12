965dd36f4f9c1c3ab1611518da13055c74d73c13
Daniel <danielgunawann1997@gmail.com>
2020-12-13 04:21:53 +0700

    Add Annotation

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,11 @@
+# == Schema Information
+#
+# Table name: transactions
+#
+#  id          :bigint           not null, primary key
+#  total_price :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class Transaction < ApplicationRecord
 end
--- test/models/notification_test.rb
+++ test/models/notification_test.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: notifications
+#
+#  id         :bigint           not null, primary key
+#  message    :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require 'test_helper'
 
 class NotificationTest < ActiveSupport::TestCase
